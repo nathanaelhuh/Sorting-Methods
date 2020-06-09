0621944f322238e0f24e4746db02c5869c614627
Nathanael Huh <nathanael.huh@gmail.com>
2020-06-09 16:03:12 -0600

    Updated Bubblesort

--- BubbleSort.py
+++ BubbleSort.py
@@ -3,9 +3,25 @@ from BaseSort import *
 class BubbleSort(BaseSort):
     #Add in your bubble sorting method here
     def bubbleSort(self, list):
-        print(list)
+        for j in list:
+            for i in list:
+                if i < len(list)-1:
+                    if list[i] > list[i+1]:
+                        temp = list[i]
+                        list[i] = list[i+1]
+                        list[i+1] = temp
+
+
 #Empty list
 l = []
+print(l)
 
 bubble = BubbleSort()
 bubble.populate(l)
+print(l)
+
+swap = SwapList()
+swap.swap(l)
+print(l)
+bubble.bubbleSort(l)
+print(l)
